1de09da787af331f0156df1ac011f416a195576b
heric-hh <erhh.015@gmail.com>
2025-05-14 19:43:53 -0600

    feat: (homepage) obtaining name from ProfileScreen TextField

--- lib/src/presentation/pages/home_screen.dart
+++ lib/src/presentation/pages/home_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _userName = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserName(); // Cargar el nombre del usuario al iniciar
+  }
+
+  Future<void> _loadUserName() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _userName =
+          prefs.getString('user_name') ?? 'Usuario'; // Valor predeterminado
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('HomeScreen')));
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Bienvenido, $_userName',
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
 }
